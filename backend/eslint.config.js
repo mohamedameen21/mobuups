38388de8151af83cbea6970f4570728ed9093a15
Ameen <afmeen3@gmail.com>
2026-07-22 06:53:43 +0530

    chore(backend): add ESLint + Prettier + explicit typecheck script

---
+++ backend/eslint.config.js
@@ -0,0 +1,16 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.ts'],
+    extends: [js.configs.recommended, tseslint.configs.recommended, eslintConfigPrettier],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+]);
